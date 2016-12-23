aa609fcd6a6a4933733e5f2a6147b0b7a9f2fa68
twesix <278227739@qq.com>
2016-12-23 13:53:57 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
 $(function()
 {
     $("[data-correspond-with='navbar']").hide();
-    $("a[href='"+location.hash+"']").parent().addClass('active');
-    $(location.hash).show(300);
+    if(location.hash)
+    {
+        $("a[href='"+location.hash+"']").parent().addClass('active');
+        $(location.hash).show(300);
+    }
+    else
+    {
+        activate_navbar_item({target:$("#navbar-top ul>li>a:first")[0]});
+    }
     $(document.body).css('visibility','visible');
 
     $("#navbar-top ul>li").click(activate_navbar_item);
     $("#days").text(get_days());
 });
+
+function activate_navbar_item(e)
+{
+    if(e.target)
+    {
+        $("#navbar-top ul>li").removeClass('active');
+        $(e.target).parent().addClass('active');
+        var id=$(e.target,"a")[0].href.split('#')[1];
+        $("[data-correspond-with='navbar']").hide();
+        $("#"+id).show(300);
+    }
+}
+
+function get_days()
+{
+    var date=new Date();
+    date.setFullYear(1996);
+    date.setMonth(9);
+    date.setDate(10);
+    date.setHours(5);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    return parseInt((new Date().getTime()-date.getTime())/(1000*3600*24));
+}
