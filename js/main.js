e68c180fadc4d7c632ad0367fdcbe322ac2a44dd
twesix <278227739@qq.com>
2016-12-23 13:30:04 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(function()
 {
     $("[data-correspond-with='navbar']").hide();
-    $("#basic").show();
+    $("a[href='"+location.hash+"']").parent().addClass('active');
+    $(location.hash).show(300);
     $(document.body).css('visibility','visible');
 
     $("#navbar-top ul>li").click(activate_navbar_item);
